bbf28725b845bfab8b6b7f8014ebb81c66741dd8
hycner <hycner@users.noreply.github.com>
2021-01-29 17:19:08 -0800

    testing artifacts

--- scripts/artifacts-1.sh
+++ scripts/artifacts-1.sh
@@ -1,8 +1,11 @@
 BUILD_DIR="$(dirname $(dirname $(realpath $0 )))/build"
+ARTIFACTS_DIRNAME="artifacts"
+ARTIFACTS_DIR="../build/$ARTIFACTS_DIRNAME"
 
 echo "artifacts one"
 
-echo "artifacts file" > "$BUILD_DIR/artifacts"
+mkdir $ARTIFACTS_DIR
+echo "artifacts file" > "$BUILD_DIR/artifacts/artifacts-file"
 
-type zip
-type unzip
+cd $BUILD_DIR
+zip -r "$BUILD_DIR/artifacts.zip" $ARTIFACTS_DIRNAME
--- scripts/artifacts-2.sh
+++ scripts/artifacts-2.sh
@@ -1,9 +1,14 @@
 BUILD_DIR="$(dirname $(dirname $(realpath $0 )))/build"
+ARTIFACTS_DIR="$BUILD_DIR/artifacts"
+ARTIFACTS_FILE="$BUILD_DIR/artifacts/artifacts-file"
+ARTIFACTS_ZIP="$BUILD_DIR/artifacts.zip"
 
 echo "artifacts two"
 
-cat artifacts
-cat $BUILD_DIR/artifacts
+mv $ARTIFACTS_ZIP "build/$ARTIFACTS_ZIP"
 
-echo "artifacts two" >> "$BUILD_DIR/artifacts"
+unzip $ARTIFACTS_ZIP -d $BUILD_DIR
+
+cat $BUILD_DIR/artifacts
+echo "artifacts two" >> $ARTIFACTS_FILE
 cat $BUILD_DIR/artifacts
