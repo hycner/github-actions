89075ffa8d34eb72c00344d4418bdb5c4e108346
hycner <hycner@users.noreply.github.com>
2021-02-06 15:37:47 -0800

    work on artifacts

--- scripts/artifacts-2.sh
+++ scripts/artifacts-2.sh
@@ -1,14 +1,15 @@
 BUILD_DIR="$(dirname $(dirname $(realpath $0 )))/build"
 ARTIFACTS_DIR="$BUILD_DIR/artifacts"
 ARTIFACTS_FILE="$BUILD_DIR/artifacts/artifacts-file"
-ARTIFACTS_ZIP="$BUILD_DIR/artifacts.zip"
+ARTIFACTS_ZIP_NAME="artifacts.zip"
+ARTIFACTS_ZIP="$BUILD_DIR/$ARTIFACTS_ZIP_NAME"
 
 echo "artifacts two"
 
 ls
 
 #mkdir $BUILD_DIR
-mv $ARTIFACTS_ZIP $BUILD_DIR
+mv $ARTIFACTS_ZIP_NAME $BUILD_DIR
 
 unzip $ARTIFACTS_ZIP -d $BUILD_DIR
 
