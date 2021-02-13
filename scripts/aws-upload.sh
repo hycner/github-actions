3768c484818ce4667d9f4e4ae1d225ce07ddef0c
hycner <hycner@users.noreply.github.com>
2021-02-12 18:47:16 -0800

    add aws example

---
+++ scripts/aws-upload.sh
@@ -0,0 +1,7 @@
+BUILD_DIR="$(dirname $(dirname $(realpath $0 )))/build"
+FILENAME="example-file.md"
+FILE_PATH="$BUILD_DIR/$FILENAME"
+
+echo "the example file" > $FILE_PATH
+
+aws s3 cp $FILE_PATH "s3://$AWS_BUCKET/"
